4d8b859619bb584da6d98430ec281436a1b26da1
kevat <kevatrajat29@gmail.com>
2023-07-15 15:59:42 +0530

    Done

--- app/src/main/java/com/example/the_hotel/Hotel.kt
+++ app/src/main/java/com/example/the_hotel/Hotel.kt
@@ -5,10 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 
 class Hotel : Fragment() {
-    private lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
