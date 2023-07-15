032e878f6a229cf43885182bc613504c26c7f9ad
kevat <kevatrajat29@gmail.com>
2023-07-15 15:49:35 +0530

    Done

--- app/src/main/java/com/example/the_hotel/Hotel.kt
+++ app/src/main/java/com/example/the_hotel/Hotel.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+import androidx.recyclerview.widget.RecyclerView
 
 class Hotel : Fragment() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_hotel, container, false)
+        val view = inflater.inflate(R.layout.fragment_hotel,container,false)
+        return view
     }
+
 }
